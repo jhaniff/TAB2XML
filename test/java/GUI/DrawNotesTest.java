993fa0e5ebb4460bd149db8fbe2a17c62bae5d54
Joshua Hanif <90522357+jhaniff@users.noreply.github.com>
2023-12-06 14:15:28 -0500

    Uploaded test files

---
+++ test/java/GUI/DrawNotesTest.java
@@ -0,0 +1,53 @@
+package GUI;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import musicxml.parsing.*;
+import GUI.DrawNotes;
+import javafx.scene.layout.Pane;
+
+@SuppressWarnings("unused")
+public class DrawNotesTest {
+
+	private MainViewController mvc;
+	
+	public void setMainViewController(MainViewController mvcInput) {
+		mvc = mvcInput; // Set main view controller
+	}
+	
+	@Test
+	public void testDrawNotes() {
+		//To do: Test measure list is correct from input(Parser and measureList are initialized already)
+		Parser p = new Parser(mvc.converter.getMusicXML());
+		List<Measure> measureList = p.getMeasures();
+		
+		//Testing if DrawNotes methods are correct
+		Note guitarNote = new Note(2, 2, "eighth", 4.0, 5, new Tremolo(null, 0), new TimeModification(-1, -1), new Slide(null, -1), new HammerOn(-1, null, null)); // Initialized guitar note with random values for parameters
+		Unpitched pitch = new Unpitched('2', 5); // Initialized random pitch with random parameters for drum
+		Note drumNote = new Note(pitch, 2, "A2", 4, "eighth", "down", "B2", 5, 5, new Tremolo(null, 0), new TimeModification(-1, -1), new Slide(null, -1), new HammerOn(-1, null, null), null, new Slur(0, null, null), new PullOff(0, null, null)); // Initialized drum note with random values for parameters
+
+		Pane pane = new Pane(); // Initialized dummy pane
+		DrawNotes drawGuitarNote = new DrawNotes(pane, 40.0, 50.0, guitarNote, "guitar", 15, 0, false, guitarNote, 0); // Initialized dummy guitar note
+		DrawNotes drawDrumSetNote = new DrawNotes(pane, 60.0, 70.0, drumNote, "drumset", 15, 0, false, drumNote, 0); // Initialized dummy drum note
+		
+		//Testing if attributes for the draw notes methods are correct
+		assertEquals(40.0, drawGuitarNote.getX());
+		assertEquals(50.0, drawGuitarNote.getY());
+		assertEquals(guitarNote, drawGuitarNote.getNote());
+		assertEquals(pane, drawGuitarNote.getPane());
+		assertEquals(pane, drawDrumSetNote.getPane());
+		assertEquals(drumNote, drawDrumSetNote.getNote());
+		assertEquals(60.0, drawDrumSetNote.getX());
+		assertEquals(70.0, drawDrumSetNote.getY());
+		
+	}
+	
+}
---
+++ test/java/parser/TestTemplate.java
@@ -0,0 +1,49 @@
+package parser;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.junit.jupiter.api.*;
+
+import musicxml.parsing.*;
+
+public class TestTemplate {	
+	/*
+	 * When creating a new test class,
+	 * 		1. Replace all current imports with the ones above
+	 * 		2. Copy the block below into your new class, replacing the auto-created test method
+	 * 		3. Uncomment the block you just pasted
+	 * 		4. Create test cases using the template block shown below the setUp method
+	 * 			Remember to change the method name!	
+	 * 			Be sure to test both wikiDrumTab and wikiGuitarTab for each method you are testing
+	 */
+//	protected Parser wikiDrumTab, wikiGuitarTab;
+//	@BeforeEach
+//	void setUp() throws Exception {
+//		//Get the OS
+//        String OS = System.getProperty("os.name").toLowerCase();
+//        //Get the filepath of all MusicXML files, based on the device's OS
+//        String filePath = new File("").getAbsolutePath();
+//        //Insert "\\" in path
+//        if (OS.contains("win")) {
+//            filePath = filePath.concat("\\src\\test\\resources\\musicXMLFiles\\");
+//        }
+//        //Assumed to be unix (mac or linux)
+//        //Insert "/" in path
+//        else {
+//            filePath = filePath.concat("/src/test/resources/musicXMLFiles/");
+//        }
+//		wikiDrumTab = new Parser(Files.readString(Paths.get(filePath.concat("wikiDrumTab.txt"))));
+//		wikiGuitarTab = new Parser(Files.readString(Paths.get(filePath.concat("wikiGuitarTab.txt"))));
+//	}
+//	
+//	@Test
+//	public void testInsertNameHere() {
+//		assertEquals(expectedValue, this.wikiGuitarTab.methodNameAsNeeded());
+//		assertEquals(expectedValue, this.wikiDrumTab.methodNameAsNeeded());
+//	}
+}
